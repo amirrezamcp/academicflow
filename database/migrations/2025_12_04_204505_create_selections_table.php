b3db39c8aebef25bcd59c15a592ca526b62665f4
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2025-12-05 00:38:20 +0330

    Create Migrations

---
+++ database/migrations/2025_12_04_204505_create_selections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('selections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('students')->cascadeOnDelete();
+            $table->foreignId('presentation_id')->constrained('presentations')->cascadeOnDelete();
+            $table->float('score')->nullable();
+            $table->unsignedSmallInteger('year_education')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('selections');
+    }
+};
