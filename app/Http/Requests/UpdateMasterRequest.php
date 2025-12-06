374b41aad33e4c7d62d254f77d318fc94aebd82e
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2025-12-06 22:57:41 +0330

    Create UpdateMasterRequest

---
+++ app/Http/Requests/UpdateMasterRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateMasterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'graduation' => 'nullable|string|max:50',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'نام',
+            'graduation' => 'مدرک',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'لطفاً نام استاد را وارد کنید.',
+            'name.max' => 'نام نباید بیشتر از 100 کاراکتر باشد.',
+            'graduation.max' => 'مدرک نباید بیشتر از 50 کاراکتر باشد.',
+        ];
+    }
+}
