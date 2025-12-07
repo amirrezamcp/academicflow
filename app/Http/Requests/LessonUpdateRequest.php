ee530dcaa229386b66bebf7d9829b0fa21bfeb95
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2025-12-07 14:46:22 +0330

    Create LessonUpdateRequest

---
+++ app/Http/Requests/LessonUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LessonUpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'unit' => 'required|integer|min:1',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'نام',
+            'unit' => 'واحد',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'وارد کردن نام درس الزامی است.',
+            'unit.required' => 'تعداد واحد الزامی است.',
+            'unit.integer'  => 'تعداد واحد باید عدد صحیح باشد.',
+            'unit.min'      => 'تعداد واحد باید حداقل ۱ باشد.',
+        ];
+    }
+}
