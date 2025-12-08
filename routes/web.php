775dd1bc2ba5410fdb3c880891fb2a4137ce54e6
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2025-12-08 23:28:54 +0330

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MasterController;
+use App\Http\Controllers\LessonController;
+use App\Http\Controllers\PresentationController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SelectionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect()->route('masters.index'));
+
+Route::resource('masters', MasterController::class);
+Route::resource('lessons', LessonController::class);
+Route::resource('presentations', PresentationController::class);
+Route::resource('students', StudentController::class);
+Route::resource('selections', SelectionController::class);
